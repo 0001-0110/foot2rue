f087550a8fb737998f0b1889decbcb63f3f33491
Rémi TROTEL <remi.trotel@epita.fr>
2023-05-31 16:41:44 +0200

    Reversed changes and put all control extensions back in the WF project

--- foot2rue.WF/Extensions/ControlExtensions.cs
+++ foot2rue.WF/Extensions/ControlExtensions.cs
@@ -1,3 +1,6 @@
+using LostInLocalization;
+using System.Globalization;
+
 namespace foot2rue.WF.Extensions;
 
 internal static class ControlExtensions
@@ -17,7 +20,7 @@ internal static class ControlExtensions
         parent?.Controls.Add(control);
     }
 
-    #region
+    #region Paint
 
     private static void control_PaintSelected(object? sender, PaintEventArgs e)
     {
@@ -85,4 +88,77 @@ internal static class ControlExtensions
         control.FindChildrenOfType(ref controls, recursive);
         return controls;
     }
+
+    #region Localization
+
+    public static void SetLocalizationString(this Control control, string localizationString)
+    {
+        control.Tag = localizationString;
+        control.LoadLocalization(false);
+    }
+
+    public static void SetLocalizationString(this ToolStripItem item, string localizationString)
+    {
+        item.Tag = localizationString;
+        item.LoadLocalization();
+    }
+
+    public static void LoadLocalization(this Control control, bool recursive = true)
+    {
+        control.LoadLocalization(LocalizationService.Instance.Culture, recursive);
+    }
+
+    public static void LoadLocalization(this Control control, CultureInfo culture, bool recursive = true)
+    {
+        if (control.Tag is string localizationString)
+            control.Text = LocalizationService.Instance.GetLocalizedString(localizationString);
+
+        if (!recursive)
+            return;
+
+        // Recursive call to refresh everything inside this component
+        if (control is ToolStrip toolStrip)
+        {
+            foreach (ToolStripItem child in toolStrip.Items)
+            {
+                if (child is ToolStripMenuItem menuItem)
+                    menuItem.LoadLocalization(culture, recursive);
+                else
+                    child.LoadLocalization(culture);
+            }
+
+            return;
+        }
+
+        foreach (Control child in control.Controls)
+            child.LoadLocalization(culture, recursive);
+    }
+
+    public static void LoadLocalization(this ToolStripItem item, bool recursive = true)
+    {
+        if (item is ToolStripMenuItem menuItem)
+            menuItem.LoadLocalization(LocalizationService.Instance.Culture, recursive);
+        else
+            item.LoadLocalization(LocalizationService.Instance.Culture);
+    }
+
+    public static void LoadLocalization(this ToolStripItem item, CultureInfo culture)
+    {
+        if (item.Tag is string localizationString)
+            item.Text = LocalizationService.Instance.GetLocalizedString(localizationString);
+    }
+
+    public static void LoadLocalization(this ToolStripMenuItem item, CultureInfo culture, bool recursive = true)
+    {
+        if (item.Tag is string localizationString)
+            item.Text = LocalizationService.Instance.GetLocalizedString(localizationString);
+
+        if (!recursive)
+            return;
+
+        foreach (ToolStripMenuItem child in item.DropDownItems)
+            child.LoadLocalization(culture, recursive);
+    }
+
+    #endregion
 }
